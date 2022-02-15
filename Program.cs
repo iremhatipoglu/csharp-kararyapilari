20db942ab4bdbbed6208b571cf2e4a425b80f4e2
iremhatipoglu <62469438+iremhatipoglu@users.noreply.github.com>
2022-02-15 10:02:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kararYapilariPatika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /********** Patika C# Modülü Karar Yapıları Ders Notları **********/
+
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 12)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time > 11 && time < 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else if (time >= 18 && time <= 21)
+            {
+                Console.WriteLine("İyi akşamlar");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+
+            /*******************************************************************/
+
+            string sonuc;
+
+            sonuc = time >= 6 && time < 12 ? "Günaydın" : time > 11 && time < 18 ?
+                "İyi günler" : time >= 18 && time <= 21 ? "İyi akşamlar" : "İyi geceler";
+            Console.WriteLine(sonuc);
+
+
+        }
+    }
+}
